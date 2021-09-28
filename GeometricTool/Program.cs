a9b25df65f168d3570c3f7e6d3a1c58cd1526635
soladitan <sablik2u@yahoo.com>
2021-09-28 07:11:42 +0100

    Add project files.

---
+++ GeometricTool/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace GeometricTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var square = new Square() { Width = 2 }; // We will instantiate the width
+            var triangle = new Triangle() { Base = 2, Height = 5 };
+
+            square.Display();  //USe the dispaly function in the abstract class but just go and look for the definition of GetArea() in the Square class.
+            triangle.Display();
+        }
+    }
+
+    abstract class Shape
+    {
+        public abstract int GetArea();
+        public void Display()
+        {
+            Console.WriteLine("The area is {0}", GetArea());
+        }
+    }
+    class Square : Shape
+    {
+        public int Width;
+        public override int GetArea() //Append the keyword override to this line
+        {
+            return Width * Width;
+        }
+    }
+
+    class Triangle : Shape
+    {
+        public int Base;
+        public int Height;
+        public override int GetArea()
+        {
+            return (Base * Height) / 2;
+        }
+    }
+}
